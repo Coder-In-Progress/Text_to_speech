1d5590328f13f760702e4c587633b1a664c0b252
Coder-In-Progress <79266681+Coder-In-Progress@users.noreply.github.com>
2021-04-12 09:40:53 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+from __future__ import print_function
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+import datetime
+
+plus_var = (var1 + var2)
+var1 = input("Type what you want the computer to say \n")
+var2 = input("Is there anything else? Type n/a if you have nothing to say")
+
+if var2 == "n/a"
+  Continue
+
+
+def speak(plus_var):
+	tts = gTTS(text=plus_var, lang="en")
+	filename = "recording.mp3"
+	tts.save(filename)
+	playsound.playsound(filename)
