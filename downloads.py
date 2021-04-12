b90b6d795ca849a3b2995366fa86ff7d92566193
Coder-In-Progress <79266681+Coder-In-Progress@users.noreply.github.com>
2021-04-12 09:53:44 -0400

    Create downloads.py

---
+++ downloads.py
@@ -0,0 +1,9 @@
+# This must downloaded in the Command Prompt (CMD)
+# Copy and paste each line seperately in the CMD
+pip install SpeechRecognition
+pip install gTTS
+pip install playsound
+pip install pyaudio
+# If Pyaudio does not work...
+# Go to this link https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyaudio 
+# Then download the correct type of python pyaudio version
